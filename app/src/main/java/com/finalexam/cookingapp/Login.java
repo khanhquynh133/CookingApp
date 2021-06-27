e6e0f2d36e5971b6c5f178cbd943adffd8c63c23
Nguyen Ngoc Huyen <45890409+Noragamiiii@users.noreply.github.com>
2021-06-27 23:02:48 +0700

    validate input

--- app/src/main/java/com/finalexam/cookingapp/Login.java
+++ app/src/main/java/com/finalexam/cookingapp/Login.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,22 +12,57 @@ import androidx.appcompat.app.AppCompatActivity;
 public class Login extends AppCompatActivity {
     TextView register;
     Button login;
+    private EditText email, password;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         register = findViewById(R.id.tv_register);
+        email = findViewById(R.id.etemailL);
+        password = findViewById(R.id.etpassL);
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(Login.this,Register.class));
+                startActivity(new Intent(Login.this, Register.class));
             }
         });
         login = findViewById(R.id.btn_loginL);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Login.this,HomePage.class));
+            public void onClick(View view) {
+                loginBtn(view);
             }
         });
+    }
+    public Boolean validateEmail(){
+        String valEmail = email.getText().toString().trim();
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+$";
+        if(valEmail.isEmpty()){
+            email.setError("Email cannot be empty");
+            return false;
+        }else if(!valEmail.matches(emailPattern)){
+            email.setError("Invalid email address");
+            return false;
+        }else{
+            email.setError(null);
+            return true;
+        }
+    }
+    public  Boolean validatePassword(){
+        String valPassword = password.getText().toString().trim();
+        if(valPassword.isEmpty()){
+            password.setError("Password cannot be empty");
+            return false;
+        }else{
+            password.setError(null);
+            return true;
+        }
+    }
+    public void loginBtn(View view){
+        if(!validateEmail() | !validatePassword()){
+            return;
+        }else{
+            Intent switchActivityIntent = new Intent(this, HomePage.class);
+            startActivity(switchActivityIntent);
+        }
     }
 }
--- app/src/main/java/com/finalexam/cookingapp/Register.java
+++ app/src/main/java/com/finalexam/cookingapp/Register.java
@@ -3,15 +3,21 @@ package com.finalexam.cookingapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Register extends AppCompatActivity {
     TextView login;
+    private EditText email, username, password;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        email = findViewById(R.id.etemailR);
+        username = findViewById(R.id.etnameR);
+        password = findViewById(R.id.etpassR);
+
         login = findViewById(R.id.tv_login);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -19,5 +25,48 @@ public class Register extends AppCompatActivity {
                 startActivity(new Intent(Register.this,Login.class));
             }
         });
+    }
+    public Boolean validateEmail(){
+        String valEmail = email.getText().toString().trim();
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+$";
+        if(valEmail.isEmpty()){
+            email.setError("Email cannot be empty");
+            return false;
+        }else if(!valEmail.matches(emailPattern)){
+            email.setError("Invalid email address");
+            return false;
+        }else{
+            email.setError(null);
+            return true;
+        }
+    }
+    public Boolean validateUsername(){
+        String valUsername = username.getText().toString().trim();
+        String whiteSpace = "\\A\\w{4,20}\\z";
+        if(valUsername.isEmpty()) {
+            username.setError("Username cannot be empty");
+            return false;
+        }else if(!valUsername.matches(whiteSpace)) {
+            username.setError("White Space in username is not allowed");
+            return false;
+        }else{
+            username.setError(null);
+            return true;
+        }
+    }
+    public  Boolean validatePassword(){
+        String valPassword = password.getText().toString().trim();
+        if(valPassword.isEmpty()){
+            password.setError("Password cannot be empty");
+            return false;
+        }else{
+            password.setError(null);
+            return true;
+        }
+    }
+    public void registerBtn(View view){
+        if(!validateEmail() | !validateUsername() | !validatePassword()){
+            return;
+        }
     }
 }
