2b510957caa9f6ef852c5a735e7cef447e5b9720
khanhquynh133 <65670847+khanhquynh133@users.noreply.github.com>
2021-06-30 15:41:49 +0700

    Update layout

--- app/src/main/java/com/finalexam/cookingapp/MainActivity.java
+++ app/src/main/java/com/finalexam/cookingapp/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,Login.class));
+                startActivity(new Intent(MainActivity.this,RememberLogin.class));
             }
         });
         register.setOnClickListener(new View.OnClickListener() {
